7002aff7274099bb2f34567721d5c9fdb00ad8f6
hatakeyamatakuma <hatakuma@icloud.com>
2023-02-17 20:49:14 +0900

    third-commit revice-prototype

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-$(function(){
+$(function() {
   const win = $(window),
-      main = $("#main"),
-      nav = $("#nav"),
-      navHeight = nav.outerHeight(),
-      navPos = nav.offset().top,
-      fixedClass = "is-fixed";
-
-      win.on("load scroll", function(){
-        const value = $(this).scrollTop();
-        if(value > navPos) {
-          nav.addClass(fixedClass);
-          main.css("margin-top", navHeight);
-          nav.css("background-color", "blue");
-        }else {
-          nav.removeClass(fixedClass);
-          main.css("margin-top", "0");
-          nav.css("background-color", "gray")
-        }
-      })
+      nav = $('.nav'),
+      navTop = nav.offset().top;
+  $(window).scroll(function(){
+    let winTop = $(this).scrollTop();
+    if(winTop >= navTop) {
+      nav.addClass("fixed")
+    }else if(winTop <= navTop) {
+      nav.removeClass("fixed")
+    }
+  })
 });

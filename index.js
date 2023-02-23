f63a638b14dc74f0f7efec621917de2be74c179a
hatakeyamatakuma <hatakuma@icloud.com>
2023-02-23 20:16:04 +0900

    fourth-commit revice-prototype

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+// $(function() {
+//   const win = $(window),
+//       nav = $('.nav'),
+//       navTop = nav.offset().top;
+//   $(window).scroll(function(){
+//     let winTop = $(this).scrollTop();
+//     if(winTop >= navTop) {
+//       nav.addClass("fixed")
+//     }else if(winTop <= navTop) {
+//       nav.removeClass("fixed")
+//     }
+//   })
+// });
+
 $(function() {
-  const win = $(window),
-      nav = $('.nav'),
-      navTop = nav.offset().top;
-  $(window).scroll(function(){
-    let winTop = $(this).scrollTop();
-    if(winTop >= navTop) {
-      nav.addClass("fixed")
-    }else if(winTop <= navTop) {
-      nav.removeClass("fixed")
+  let target = $(".nav").offset().top;
+  $(window).on("scroll", function() {
+    let currentPos = $(window).scrollTop();
+    if(currentPos > target) {
+      $(".nav").addClass('active');
+    } else{
+      $(".nav").removeClass('active');
     }
-  })
+  });
 });
